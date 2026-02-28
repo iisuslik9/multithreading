acd9ff5e5078c0b24f0e955c067f9c622c405f57
vershinina618 <73005698+vershinina618@users.noreply.github.com>
2026-02-28 14:59:52 +0700

    lab1.1 helloworld

---
+++ ConsoleAppHelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Thread myThread = new Thread(PrintHello);
+        
+        myThread.Start();
+
+        Console.WriteLine("главный поток");
+
+        myThread.Join();
+
+    }
+
+    static void PrintHello()
+    {
+        Console.WriteLine("фоновый поток: Здравствуй, мир");
+    }
+}
